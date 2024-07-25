d66cfe4c2037841971e41cf448f86c002372c1ba
sunflow-er <antjd03170@gmail.com>
2024-07-26 02:18:33 +0900

    회원가입 및 로그인을 위한 Firebase 세팅

---
+++ app/src/main/java/org/javaapp/dailylog/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.javaapp.dailylog
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
