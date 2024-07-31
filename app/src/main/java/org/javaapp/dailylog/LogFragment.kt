7ffa9f3944f6f6d3ac3008e0c2e13b3469132baf
sunflow-er <antjd03170@gmail.com>
2024-08-01 01:28:55 +0900

    메인 액티비티의 세부 프래그먼트(with, log, my) 추가

---
+++ app/src/main/java/org/javaapp/dailylog/LogFragment.kt
@@ -0,0 +1,7 @@
+package org.javaapp.dailylog
+
+import androidx.fragment.app.Fragment
+
+class LogFragment : Fragment() {
+
+}
---
+++ app/src/main/java/org/javaapp/dailylog/WithFragment.kt
@@ -0,0 +1,6 @@
+package org.javaapp.dailylog
+
+import androidx.fragment.app.Fragment
+
+class WithFragment : Fragment() {
+}
