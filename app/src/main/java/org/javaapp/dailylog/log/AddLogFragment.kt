b20a8156d8628b6af56dc3307e946d831fb0f735
sunflow-er <antjd03170@gmail.com>
2024-08-06 17:11:12 +0900

    새로운 글 작성 화면에 뒤로 가기 메뉴 추가

--- app/src/main/java/org/javaapp/dailylog/log/AddLogFragment.kt
+++ app/src/main/java/org/javaapp/dailylog/log/AddLogFragment.kt
@@ -3,9 +3,14 @@ package org.javaapp.dailylog.log
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -13,6 +18,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import org.javaapp.dailylog.Key
+import org.javaapp.dailylog.R
 import org.javaapp.dailylog.databinding.FragmentAddLogBinding
 import java.time.LocalDateTime
 
@@ -39,6 +45,25 @@ class AddLogFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 앱바 메뉴 인플레이트 및 리스너 설정
+        val menuHost : MenuHost = requireActivity()
+        val menuProvider = object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_comment, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.back_to_log -> {
+                        requireActivity().supportFragmentManager.popBackStack() // 뒤로 가기
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+        menuHost.addMenuProvider(menuProvider, viewLifecycleOwner)
 
         binding.addImage.setOnClickListener {
             // TODO 이미지 불러오기
