a24062f14e0fd0089d25ce8afe324778f0b33bd1
sunflow-er <antjd03170@gmail.com>
2024-08-09 21:44:36 +0900

    alertDialog를 통해 현재 사용자 이름과 상태메시지를 수정 및 실시간 반영하는 기능 구현

--- app/src/main/java/org/javaapp/dailylog/my/MyFragment.kt
+++ app/src/main/java/org/javaapp/dailylog/my/MyFragment.kt
@@ -2,17 +2,40 @@ package org.javaapp.dailylog.my
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import org.javaapp.dailylog.Key
+import org.javaapp.dailylog.R
 import org.javaapp.dailylog.SignInActivity
 import org.javaapp.dailylog.databinding.FragmentMyBinding
+import org.javaapp.dailylog.user.User
 
 class MyFragment : Fragment() {
     private lateinit var binding : FragmentMyBinding
+    private lateinit var currentUser : FirebaseUser
+    private lateinit var database : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        currentUser = Firebase.auth.currentUser!!
+        database = Firebase.database(Key.DB_URL).reference
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +50,77 @@ class MyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // 내 정보 가져와서 띄우기
+        database.child(Key.DB_USERS).child(currentUser.uid).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                user ?: return
+
+                binding.myProfileImage.setImageResource(R.drawable.baseline_account_box_24)
+                binding.myNameText.text = user.name
+                binding.myStatusMessageText.text = user.statusMessage
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+        // 내 정보 수정 버튼 리스너 설정
+        binding.myEditButton.setOnClickListener {
+            showEditProfileDialog()
+        }
+
+        // 로그아웃 버튼
 
     }
-
 
     companion object {
         fun newInstance() : MyFragment {
             return MyFragment()
         }
+    }
+
+    private fun showEditProfileDialog() {
+        // dialog 생성에 필요한 뷰 정보들
+        val dialog = LayoutInflater.from(context).inflate(R.layout.dialog_edit_my, null)
+        val profileImage = dialog.findViewById<ImageView>(R.id.edit_profile_image)
+        val nameEdit = dialog.findViewById<EditText>(R.id.edit_name_edit)
+        val statusMessageEdit = dialog.findViewById<EditText>(R.id.edit_status_message_edit)
+        val signOutButton = dialog.findViewById<Button>(R.id.edit_sign_out_button)
+
+        // 현재 데이터를 기본값으로 세팅
+        profileImage.setImageResource(R.drawable.baseline_account_box_24)
+        nameEdit.setText(binding.myNameText.text)
+        statusMessageEdit.setText(binding.myStatusMessageText.text)
+
+        // dialog 생성 및 띄우기, 이벤트 처리
+        AlertDialog.Builder(requireContext())// 빌더
+            .setTitle("내 정보 수정") // 제목
+            .setView(dialog)// 뷰
+            .setPositiveButton("저장") { dialog, _ -> // 저장 버튼을 눌렀을 때
+                // 수정한 정보 가져오기
+                val newName = nameEdit.text.toString()
+                val newStatusMessage = statusMessageEdit.text.toString()
+
+                // 파이어베이스 데이터베이스 유저 정보에 반영, 업데이트
+                val userInfoUpdate = mapOf(
+                    "name" to newName,
+                    "statusMessage" to newStatusMessage
+                )
+                database.child(Key.DB_USERS).child(currentUser.uid).updateChildren(userInfoUpdate)
+
+                // my 화면에 반영, 업데이트
+                binding.myNameText.text = newName
+                binding.myStatusMessageText.text = newStatusMessage
+
+                // dialog 닫기
+                dialog.dismiss()
+            }.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create() // 생성
+            .show() // 보여주기
     }
 }
